53cd12f93a204685c957c02afc77ea61c450e5d0
isabelapiana <isabelapiana91@gmail.com>
2022-06-03 16:21:21 -0300

    UPDATE CRUD

--- src/main/java/org/generation/lojadegames/repository/ProdutoRepository.java
+++ src/main/java/org/generation/lojadegames/repository/ProdutoRepository.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 
 import org.generation.lojadegames.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
 	public List<Produto> findAllByNomeContainingIgnoreCase (String nome);
 	public List <Produto> findByPrecoLessThan (Double preco);
